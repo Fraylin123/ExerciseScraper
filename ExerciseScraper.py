9cc8909893e3b7db77de6a16621eddac0640c399
Fraylin <fraylinayala321@gmail.com>
2024-12-24 00:01:02 -0500

    Instead of clicking use driver.get() since is not working lol

--- ExerciseScraper.py
+++ ExerciseScraper.py
@@ -17,7 +17,7 @@ chromeOptions = Options()
 chromeOptions.add_argument("--disable-gpu")
 chromeOptions.add_argument("--disable-software-rasterizer") 
 
-chromeOptions.add_argument("--headless")
+##chromeOptions.add_argument("--headless")
 ##
 
 ##Start the driver
@@ -40,48 +40,61 @@ for i in range(len(exerciseLinks)):
 
 
 print(len(exerciseUrls))
+exerciseUrls = exerciseUrls[1:] ##temporary test
 
 data = {}
 
 
-def getAttributes(aElements, exercisesNames):
-    try:
-        aElementsUrls = [element.get_attribute("href") for element in aElements]
-        #print("Len of the exercises: ", len(aElementsUrls))
-       
-        global data 
-        index = 0
-        for elementUrl in aElementsUrls:
-            driver.get(elementUrl)
-            time.sleep(2)
 
-            primaryMuscle = driver.find_element(By.CSS_SELECTOR, "div.field-item.even a").text
-            otherA = driver.find_elements(By.CSS_SELECTOR, 'div.node-stats-block ul li')[1:]
-            otherAValues = [attribute.text.split('\n')[1] for attribute in otherA]
+
+def getAttributes():
+    try:
+        nextButton = "placeholder"
+        while (len(nextButton) > 0):
+            nextButton = driver.find_elements(By.CSS_SELECTOR, ".pager-next a")
+            print("Lenght of nextButton is: ", len(nextButton))
             
-            try:
-                youtubeURL = driver.find_element(By.CSS_SELECTOR, 'div.video.responsive-embed.widescreen .video-wrap iframe').get_attribute("src")
-            except NoSuchElementException:
-                youtubeURL = ''
-                
-            if ("youtube" in youtubeURL):
-                print("URL is: ", youtubeURL)
-                pattern = r"https://www\.youtube\.com/embed/([^?&]+)"
-                vidID = re.search(pattern,youtubeURL).group(1)
-            else:
-                vidID=None
-            print("Current exercise: " + exercisesNames[index])
+            subpageExercises = driver.find_elements(By.CSS_SELECTOR, "div.cell.small-12.bp600-6 .node-title a")
+            subpageExercisesNames = [currE.text for currE in subpageExercises] ##Get all exercise names
+            
+            aElementsUrls = [element.get_attribute("href") for element in subpageExercises]
+            #print("Len of the exercises: ", len(aElementsUrls))
         
-            data[exercisesNames[index]] = {
-            "primaryMuscle":primaryMuscle,
-            "type":otherAValues[0],
-            "equipment":otherAValues[1],
-            "level":otherAValues[4],
-            "secondaryMuscles":otherAValues[5],
-            "image": vidID
-            }
-           
-            index+=1
+            global data 
+            index = 0
+            for elementUrl in aElementsUrls:
+                driver.get(elementUrl)
+                time.sleep(2)
+
+                primaryMuscle = driver.find_element(By.CSS_SELECTOR, "div.field-item.even a").text
+                otherA = driver.find_elements(By.CSS_SELECTOR, 'div.node-stats-block ul li')[1:]
+                otherAValues = [attribute.text.split('\n')[1] for attribute in otherA]
+                
+                try:
+                    youtubeURL = driver.find_element(By.CSS_SELECTOR, 'div.video.responsive-embed.widescreen .video-wrap iframe').get_attribute("src")
+                except NoSuchElementException:
+                    youtubeURL = ''
+                    
+                if ("youtube" in youtubeURL):
+                    print("URL is: ", youtubeURL)
+                    pattern = r"https://www\.youtube\.com/embed/([^?&]+)"
+                    vidID = re.search(pattern,youtubeURL).group(1)
+                else:
+                    vidID=None
+                    
+                print("Current exercise: " + subpageExercisesNames[index])
+            
+                data[subpageExercisesNames[index]] = {
+                "primaryMuscle":primaryMuscle,
+                "type":otherAValues[0],
+                "equipment":otherAValues[1],
+                "level":otherAValues[4],
+                "secondaryMuscles":otherAValues[5],
+                "image": vidID
+                }
+            
+                index+=1
+            nextButton[0].click()
     except Exception as e:
         print(f"The error is inside the function: {e}")
 
@@ -93,14 +106,28 @@ for exerciseUrl in exerciseUrls: # For each subpage
 
     try:
         title = driver.find_element(By.TAG_NAME, 'h1').text
-       ## print(f"Title of the category: {title}")
-        subpageExercises = driver.find_elements(By.CSS_SELECTOR, "div.cell.small-12.bp600-6 .node-title a")
+        print(f"Title of the category: {title}")
+        nextButton = driver.find_elements(By.CSS_SELECTOR, "li.pager-next a")
+        nextLink = nextButton[0].get_attribute("href")
+        print("The text is: ", nextButton[0].text)
+        print(len(nextButton))
+        if (len(nextButton) > 0):
+            driver.get(nextLink)
+            exerciseE=driver.find_element(By.CSS_SELECTOR, "div.cell.small-12.bp600-6 .node-title a")
+            exerciseName = exerciseE.text
+            print("Exercise name is: ", exerciseName)
+
+        break
+        
+        
+    
+        
         #print("Len of subpage: " , len(subpageExercises))
-        subpageExercisesNames = [currE.text for currE in subpageExercises] ##Get all exercise names
+        
        # print("Len of subpage names: " , len(subpageExercisesNames))
         #print(subpageExercisesNames)
         #print(data)
-        getAttributes(subpageExercises, subpageExercisesNames)
+        getAttributes()
         counter+=1
         if counter == 3:
             print(data)
