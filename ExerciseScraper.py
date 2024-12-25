0e898adb5e27ce0bd0bf5550f20643269430e246
Fraylin <fraylinayala321@gmail.com>
2024-12-25 16:33:21 -0500

    Now able to insert data to MongoDB Atlas, schema needs work

--- ExerciseScraper.py
+++ ExerciseScraper.py
@@ -1,22 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
-import pymongo
+from pymongo import MongoClient
 import time
 import re
 import pandas as pd
 
+client = MongoClient("mongodb+srv://developer:developer123@exercisecluster.9sixn.mongodb.net/?retryWrites=true&w=majority&appName=exerciseCluster")
+db = client["exercisesDb"]
+collection = db["exercises"]
+
 data = {}
 
 def getPage(url):
-    base_url = "https://www.muscleandstrength.com"
+    baseUrl= "https://www.muscleandstrength.com"
     if not url.startswith("http"):
-        url = f"{base_url}{url}"
+        url = f"{baseUrl}{url}"
 
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    time.sleep(1)
-    r = requests.get(url, headers=headers)
-    r.raise_for_status()
-    return r.text
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
+    try:
+        r = requests.get(url, headers=headers)
+        r.raise_for_status()
+        time.sleep(1.5)
+        return r.text
+    except requests.exceptions.HTTPError as e:
+        print(f"Error encountered: {e}")
 
 #Scrapes individual exercises from each muscle category
 def individualExercise(url):
@@ -44,8 +51,6 @@ def individualExercise(url):
                 pattern =  r"https://www\.youtube\.com/embed/([^?&]+)"
                 vidID = re.search(pattern, youtubeURL).group(1)
                 print("Vid id is: ", vidID)
-            
-                
         
         return {
             "primaryMuscle":primaryMuscle,
@@ -53,7 +58,7 @@ def individualExercise(url):
             "equipment":otherAValues[1],
             "level":otherAValues[4],
             "secondaryMuscles":otherAValues[5],
-            "image": vidID
+            "video": vidID
         }
     
     except Exception as e:
@@ -74,13 +79,17 @@ def getCategory(url):
         
 
         for exerciseUrl in exercisesUrls:
+            data = {}
             exerciseData = individualExercise(exerciseUrl)
             print (exerciseData)
             if exerciseData:
                 data[exercisesNames[index]] = exerciseData
+                collection.update_one({"_id": exercisesNames[index]}, {"$set": exerciseData}, upsert=True )
                 index+=1
+            
+            
         
-        print(data)
+        
         
 
         nextButton = soup.select_one(".pager-next a")
@@ -107,7 +116,7 @@ for i in range(len(ecLinks)):
     if (link not in ecUrls and (link != "https://www.muscleandstrength.com/exercises/palmar-fascia" and link != "https://www.muscleandstrength.com/exercises/plantar-fascia")): #Exclude these "muscle" groups
         ecUrls.append(link)
 
-ecUrls = ecUrls[:22]
+ecUrls = ecUrls[19:22]
 
 
 for link in ecUrls:
