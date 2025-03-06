239222c82492a24545bfe898d04f0b4eab4b7dc0
fraylin <fraylinayala321@gmail.com>
2025-03-05 23:43:00 -0500

    Formatting updates

--- ExerciseScraper.py
+++ ExerciseScraper.py
@@ -4,10 +4,12 @@ from pymongo import MongoClient
 import time
 import re
 
+#Establishing MongoDB connection
 client = MongoClient("mongodb+srv://developer:developer123@exercisecluster.9sixn.mongodb.net/?retryWrites=true&w=majority&appName=exerciseCluster")
 db = client["exercisesDb"]
 collection = db["exercises"]
 
+#Initial request to the main website
 def getPage(url):
     baseUrl= "https://www.muscleandstrength.com"
     if not url.startswith("http"):
@@ -22,16 +24,17 @@ def getPage(url):
     except requests.exceptions.HTTPError as e:
         print(f"Error encountered: {e}")
 
-#Scrapes individual exercises from each muscle category
+#Function that scrapes individual exercises from each muscle category
 def individualExercise(url):
     html = getPage(url)
     soup = BeautifulSoup(html, "html.parser")
     try:
         primaryMuscle = soup.select_one("div.field-item.even a").text.strip()
         otherA = soup.select("div.node-stats-block ul li")[1:]
-    
+
         otherAValues = [attribute.contents[1] for attribute in otherA]
         otherAValues[-1] = otherA[-1].contents[2].text.strip()
+
         youtubeURL = None
         youtubeElement = soup.select_one("div.video.responsive-embed.widescreen .video-wrap iframe")
         vidID = "None"
@@ -39,7 +42,7 @@ def individualExercise(url):
         if youtubeElement:
             youtubeURL = youtubeElement.get("src", "")
             if "youtube" in youtubeURL:
-                pattern =  r"https://www\.youtube\.com/embed/([^?&]+)"
+                pattern =  r"https://www\.youtube\.com/embed/([^?&]+)" #Regular expression made to capture the "id" of the youtube video
                 vidID = re.search(pattern, youtubeURL).group(1)
                 #print("Vid id is: ", vidID)
         
@@ -55,6 +58,7 @@ def individualExercise(url):
     except Exception as e:
         print(f"Failed to parse this page ({url}): {e}")
 
+#Function to scrape data from each exercise category page (Chest, Legs, Biceps, Triceps, etc.)
 def getCategory(url):
     page = 1
     while True:
@@ -67,7 +71,6 @@ def getCategory(url):
         #print(exercisesNames)
         exercisesUrls = [exercise.get("href") for exercise in exercises]
         
-
         for exerciseUrl in exercisesUrls:
             exerciseData = individualExercise(exerciseUrl)
             #print (exerciseData)
@@ -78,17 +81,13 @@ def getCategory(url):
         nextButton = soup.select_one(".pager-next a")
         if nextButton:
             url = nextButton.get("href")
-            page+=1
+            page+=1   #If there is a next button present then it means there're more pages so request the next page
             time.sleep(1)
         else:
             break
 
 soup = BeautifulSoup(getPage("https://www.muscleandstrength.com/exercises"), "html.parser")
-
-#Exercise categories links
-#exerciseLinks = driver.find_elements(By.CSS_SELECTOR, "div.mainpage-category-list .cell a")
 ecLinks = soup.select("div.mainpage-category-list .cell a")
-
 ecUrls = []
 
 for i in range(len(ecLinks)):
@@ -102,5 +101,4 @@ for link in ecUrls:
     #print(link)
     getCategory(link)
 
-        
 print("Scraping complete")
